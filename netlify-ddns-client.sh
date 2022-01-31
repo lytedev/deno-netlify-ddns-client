3ffb0f1567f7ac10a44d2a78f895267ed964b0d0
Daniel Flanagan <daniel@lyte.dev>
2022-01-31 15:21:03 -0600

    Better max-time

--- netlify-ddns-client.sh
+++ netlify-ddns-client.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
 . "/etc/netlify-ddns-client/rc"
-curl -4 -s -X POST -u "${NETLIFY_DDNS_USERNAME}:${NETLIFY_DDNS_PASSWORD}" \
+curl -4 -s \
+  -X POST \
+  --max-time 10 \
+  -u "${NETLIFY_DDNS_USERNAME}:${NETLIFY_DDNS_PASSWORD}" \
   -L "${NETLIFY_DDNS_ENDPOINT}/v1/netlify-ddns/replace-all-relevant-user-dns-records" &
-curl -6 -s -X POST -u "${NETLIFY_DDNS_USERNAME}:${NETLIFY_DDNS_PASSWORD}" \
+curl -6 -s \
+  -X POST \
+  --max-time 10 \
+  -u "${NETLIFY_DDNS_USERNAME}:${NETLIFY_DDNS_PASSWORD}" \
   -L "${NETLIFY_DDNS_ENDPOINT}/v1/netlify-ddns/replace-all-relevant-user-dns-records" &
 wait

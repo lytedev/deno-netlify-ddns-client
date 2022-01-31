46ffd745ffee2d4f56d992a0584a8f7ab8e980b9
Daniel Flanagan <daniel@lyte.dev>
2022-01-31 15:16:53 -0600

    Perform requests in parallel

--- netlify-ddns-client.sh
+++ netlify-ddns-client.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
-source "/etc/netlify-ddns-client/rc"
+. "/etc/netlify-ddns-client/rc"
 curl -4 -s -X POST -u "${NETLIFY_DDNS_USERNAME}:${NETLIFY_DDNS_PASSWORD}" \
-	-L "${NETLIFY_DDNS_ENDPOINT}/v1/netlify-ddns/replace-all-relevant-user-dns-records"
+  -L "${NETLIFY_DDNS_ENDPOINT}/v1/netlify-ddns/replace-all-relevant-user-dns-records" &
 curl -6 -s -X POST -u "${NETLIFY_DDNS_USERNAME}:${NETLIFY_DDNS_PASSWORD}" \
-	-L "${NETLIFY_DDNS_ENDPOINT}/v1/netlify-ddns/replace-all-relevant-user-dns-records"
+  -L "${NETLIFY_DDNS_ENDPOINT}/v1/netlify-ddns/replace-all-relevant-user-dns-records" &
+wait

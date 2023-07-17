cb696cc64fbc18469a4b4a36e20904d4548daea0
Daniel Flanagan <daniel@lyte.dev>
2023-07-17 14:59:42 -0500

    Looks like Netlify's API endpoints may have slowed up?

--- netlify-ddns-client.sh
+++ netlify-ddns-client.sh
@@ -2,12 +2,12 @@
 . "/etc/netlify-ddns-client/rc"
 curl -4 -s \
   -X POST \
-  --max-time 10 \
+  --max-time 300 \
   -u "${NETLIFY_DDNS_USERNAME}:${NETLIFY_DDNS_PASSWORD}" \
   -L "${NETLIFY_DDNS_ENDPOINT}/v1/netlify-ddns/replace-all-relevant-user-dns-records" &
 curl -6 -s \
   -X POST \
-  --max-time 10 \
+  --max-time 300 \
   -u "${NETLIFY_DDNS_USERNAME}:${NETLIFY_DDNS_PASSWORD}" \
   -L "${NETLIFY_DDNS_ENDPOINT}/v1/netlify-ddns/replace-all-relevant-user-dns-records" &
 wait

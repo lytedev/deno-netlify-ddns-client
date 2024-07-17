f94e9e3bb9d4d77e14f0a79f5d396ada144bd8b5
Daniel Flanagan <daniel@lyte.dev>
2024-07-17 17:37:09 -0500

    Use a more straightforward approach that reveals errors

--- netlify-ddns-client.sh
+++ netlify-ddns-client.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
+set -eu
 . "${NETLIFY_DDNS_RC_FILE:-/etc/netlify-ddns-client/rc}"
 curl -4 -s \
   -X POST \
   --max-time 300 \
   -u "${NETLIFY_DDNS_USERNAME}:${NETLIFY_DDNS_PASSWORD}" \
-  -L "${NETLIFY_DDNS_ENDPOINT}/v1/netlify-ddns/replace-all-relevant-user-dns-records" &
+  -L "${NETLIFY_DDNS_ENDPOINT}/v1/netlify-ddns/replace-all-relevant-user-dns-records"
 curl -6 -s \
   -X POST \
   --max-time 300 \
   -u "${NETLIFY_DDNS_USERNAME}:${NETLIFY_DDNS_PASSWORD}" \
-  -L "${NETLIFY_DDNS_ENDPOINT}/v1/netlify-ddns/replace-all-relevant-user-dns-records" &
-wait
+  -L "${NETLIFY_DDNS_ENDPOINT}/v1/netlify-ddns/replace-all-relevant-user-dns-records"
